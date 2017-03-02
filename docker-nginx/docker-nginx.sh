41586114af44f4158491749f6478ace306e43df7
Tobias Wittwer <t.wittwer95@gmail.com>
2017-03-02 12:16:51 +0100

    [FEATURE] adding ability to stop the service

--- docker-nginx/docker-nginx.sh
+++ docker-nginx/docker-nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-/usr/bin/docker run -d -p 80:80 -p 443:443 \
+start() {
+    /usr/bin/docker run -d -p 80:80 -p 443:443 \
         --name nginx \
         -v /etc/nginx/conf.d  \
         -v /etc/nginx/vhost.d \
@@ -8,7 +9,7 @@
         -v /etc/docker-nginx/certs:/etc/nginx/certs:ro \
         nginx
 
-/usr/bin/docker run -d \
+    /usr/bin/docker run -d \
         --name nginx-gen \
         --volumes-from nginx \
         -v /etc/docker-nginx/nginx.tmpl:/etc/docker-gen/templates/nginx.tmpl:ro \
@@ -16,10 +17,24 @@
         jwilder/docker-gen \
         -notify-sighup nginx -watch -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
 
-/usr/bin/docker run -d \
+    /usr/bin/docker run -d \
         --name nginx-letsencrypt \
         -e "NGINX_DOCKER_GEN_CONTAINER=nginx-gen" \
         --volumes-from nginx \
         -v /etc/docker-nginx/certs:/etc/nginx/certs:rw \
         -v /var/run/docker.sock:/var/run/docker.sock:ro \
         jrcs/letsencrypt-nginx-proxy-companion
+}
+
+stop() {
+    /usr/bin/docker stop nginx
+    /usr/bin/docker stop nginx-gen
+    /usr/bin/docker stop nginx-letsencrypt
+    /usr/bin/docker rm nginx
+    /usr/bin/docker rm nginx-gen
+    /usr/bin/docker rm nginx-letsencrypt
+}
+
+case $1 in
+  start|stop) "$1" ;;
+esac
