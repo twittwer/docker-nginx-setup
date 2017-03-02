ea322ce97fbe7ae250df9d9f6f5b50965c1b1375
Tobias Wittwer <t.wittwer95@gmail.com>
2017-03-03 00:14:42 +0100

    [FEATURE] adding install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cp -r ./docker-nginx /etc/
+
+if [ -d "/etc/docker-nginx/certs" ]; then
+  sudo rm -rf /etc/docker-nginx/certs
+fi
+mkdir /etc/docker-nginx/certs
+
+cp ./docker-nginx.service /etc/systemd/system/
+
+systemctl daemon-reload
+
+#systemctl start docker-nginx.service
+
+#systemctl enable docker-nginx.service
